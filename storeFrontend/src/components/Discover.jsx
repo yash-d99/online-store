4f870ba01ad42fe16c5b846fca74f7f5e420715c
ka1979 <118313969+ka1979@users.noreply.github.com>
2023-06-06 11:53:47 -0400

    new auth changes

--- storeFrontend/src/components/Discover.jsx
+++ storeFrontend/src/components/Discover.jsx
@@ -1,7 +1,11 @@
+import LoginButton from "./LoginPage";
+
 export default function Discover() {
+
   console.log("hi");
   return (
     <>
+    <LoginButton></LoginButton>
       <h1>Test</h1>
     </>
   );
