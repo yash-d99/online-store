5a63de5d1a1c42d15e2521a238ded01f96231da5
ka1979 <118313969+ka1979@users.noreply.github.com>
2023-06-06 16:18:56 -0400

    made changes for authentication

--- storeFrontend/src/App.jsx
+++ storeFrontend/src/App.jsx
@@ -3,18 +3,19 @@ import NavigationBar from "./components/NavigationBar";
 import HomePage from "./components/HomePage";
 import Discover from "./components/Discover";
 import MyItems from "./components/MyItems";
+import Redirect from "./components/redirect";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" />
-        <Route path="/" element={<NavigationBar />}>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/Discover" element={<Discover />} />
-          <Route path="/MyItems" element={<MyItems />} />
-        </Route>
-      </Routes>
+        <Routes>
+    <Route path="/" element={<Redirect/>} />
+    <Route path="/navigation" element={<NavigationBar />}>
+      <Route path="home" element={<HomePage />} />
+      <Route path="discover" element={<Discover />} />
+      <Route path="myitems" element={<MyItems />} />
+    </Route>
+  </Routes>
     </BrowserRouter>
   );
 }
