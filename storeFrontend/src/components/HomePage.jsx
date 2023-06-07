b784e60d94e67728d21ce93d9f6a3078812a2adf
Dio Suliman <diosulimana@gmail.com>
2023-06-07 17:37:56 -0400

    Adding some style changes

--- storeFrontend/src/components/HomePage.jsx
+++ storeFrontend/src/components/HomePage.jsx
@@ -1,6 +1,8 @@
+import zephyr from "./zephyrLogo-removebg-preview.png";
 export default function HomePage() {
   return (
     <>
+      <img src={zephyr} />
       <h1>Zephyr</h1>
       <h2>Your one stop shop for all new clothing</h2>
     </>
